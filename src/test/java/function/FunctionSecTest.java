eee691118cf13e45ac84892ce88df8ea3d4a2f1c
aleks-rt <rita_aleks@mail.ru>
2022-03-14 18:02:27 +0300

    change tests for function

--- src/test/java/function/FunctionSecTest.java
+++ src/test/java/function/FunctionSecTest.java
@@ -12,21 +12,21 @@ public class FunctionSecTest {
     private final double EPS = 1.0e-5;
 
     @ParameterizedTest
-    @ValueSource(doubles = {(PI / 2), (3 * PI) / 2, (PI / 2) + 2 * PI, ((3 * PI) / 2) + (2 * PI)})
+    @ValueSource(doubles = {(PI / 2), (-PI / 2), (3 * PI) / 2, (-3 * PI) / 2, 2 * PI + (PI / 2), -(2 * PI + (PI / 2)), ((3 * PI) / 2) + (2 * PI), -((3 * PI) / 2) + (2 * PI)})
     @DisplayName("Test function by NaN result")
     public void nanTest(double x) {
         Assertions.assertEquals(FunctionSec.calc(x), Double.NaN);
     }
 
     @ParameterizedTest
-    @ValueSource(doubles = {0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9})
+    @ValueSource(doubles = {-0.5, -0.4, -0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3, 0.4, 0.5})
     @DisplayName("Test function in doubles values")
     void doublesTest(double x) {
         Assertions.assertEquals(FunctionSec.calc(x), 1 / Math.cos(x), EPS);
     }
 
     @ParameterizedTest
-    @ValueSource(doubles = {-19 * PI / 6, -5 * PI / 6, 8 * PI / 6, 17 * PI / 6})
+    @ValueSource(doubles = {PI, -PI, 2 * PI, -2 * PI, PI - 1.0, PI + 1.0, 2 * PI - 1.0, 2 * PI + 1.0, -PI - 1.0, -PI + 1.0, -2 * PI - 1.0, -2 * PI + 1.0})
     @DisplayName("Test function in PI values")
     public void piTest(double x) {
         Assertions.assertEquals(FunctionSec.calc(x), 1 / Math.cos(x), EPS);
