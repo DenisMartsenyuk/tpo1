2315384c8e3c449b153aa9dfd9879ddf56bf14b0
aleks-rt <rita_aleks@mail.ru>
2022-03-14 18:37:13 +0300

    change tests for actor

--- src/test/java/text/ActorTest.java
+++ src/test/java/text/ActorTest.java
@@ -8,6 +8,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ActorTest {
 
@@ -24,29 +25,29 @@ class ActorTest {
     }
 
     @Test
-    @DisplayName("dasfd") //todo назвать
+    @DisplayName("Test Actor move")
     public void moveTest() {
         Artur.move(a, b);
-        assertEquals("Локация Б", Artur.getLocation().getName()); // todo я бы строку поменял наверн
+        assertEquals(b.getName(), Artur.getLocation().getName());
     }
 
     @Test
-    @DisplayName("ыфвыаип") //todo назвать
+    @DisplayName("Test Actor set age")
     void setAgeTest() {
         Artur.setAge(18);
         assertEquals(18, Artur.getAge());
     }
 
     @Test
-    @DisplayName("ыфвыаип") //todo назвать
+    @DisplayName("Test Actor set incorrect age")
     void setIncorrectAgeTest() {
         Exception exception = assertThrows(Exception.class, () -> Artur.setAge(-18));
         assertEquals("Возраст должен быть положительным", exception.getMessage());
     }
 
     @Test
-    @DisplayName("ыфвыаип") //todo назвать
-    void inspectPlaceTest() {
+    @DisplayName("Test Actor inspect location")
+    void inspectLocationTest() {
         PrintStream defaultPrintStream = System.out;
         ByteArrayOutputStream output = new ByteArrayOutputStream();
         System.setOut(new PrintStream(output));
@@ -62,7 +63,7 @@ class ActorTest {
     }
 
     @Test
-    @DisplayName("ыфвыаип") //todo назвать
+    @DisplayName("Test Actor set condition")
     void setConditionTest() {
         PrintStream defaultPrintStream = System.out;
         ByteArrayOutputStream output = new ByteArrayOutputStream();
